daf20ff0322fe168ebc176fbff710cca86ee579e
VNKaiser <tandatvok16@gmail.com>
2024-12-05 15:03:10 +0700

    feat: add readme & refactor code

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -29,28 +29,28 @@ class ChatScreen extends StatelessWidget {
       ),
       body: Stack(
         children: <Widget>[
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 70),
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    // Product sale
-                    const ProductSale(
-                      name: "123 Dedap Link (Reverve)",
-                      price: "4,200,100",
-                      imageUrl: "assets/images/product_sale.png",
-                    ),
-                    // Chat
-                    const Divider(
-                      height: 1.0,
-                      thickness: 1.0,
-                      color: Color(0xFFD1D1D1),
-                    ),
-                    SizedBox(
-                      width: size.width * 0.95,
-                      child: const Column(
+          Padding(
+            padding: const EdgeInsets.only(bottom: 70),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  // Product sale
+                  const ProductSale(
+                    name: "123 Dedap Link (Reverve)",
+                    price: "4,200,100",
+                    imageUrl: "assets/images/product_sale.png",
+                  ),
+                  // Chat
+                  const Divider(
+                    height: 1.0,
+                    thickness: 1.0,
+                    color: Color(0xFFD1D1D1),
+                  ),
+                  SizedBox(
+                    width: size.width * 0.95,
+                    child: const SingleChildScrollView(
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
@@ -103,38 +103,46 @@ class ChatScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 6),
-                        width: size.width * 0.8,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                              color: const Color(0xFF797980), width: 1),
-                        ),
-                        child: const Column(children: <Widget>[
-                          Text('This property has been marked as reserved.',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w700, fontSize: 12)),
-                          Text('You may still chat for a better deal!',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w400, fontSize: 12))
-                        ]),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const SystemMessage()
+                ],
               ),
             ),
           ),
           // Bottom bar
           const ChatBottomBar(),
         ],
+      ),
+    );
+  }
+}
+
+class SystemMessage extends StatelessWidget {
+  const SystemMessage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+        width: size.width * 0.8,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: const Color(0xFF797980), width: 1),
+        ),
+        child: const Column(children: <Widget>[
+          Text('This property has been marked as reserved.',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12)),
+          Text('You may still chat for a better deal!',
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12))
+        ]),
       ),
     );
   }
